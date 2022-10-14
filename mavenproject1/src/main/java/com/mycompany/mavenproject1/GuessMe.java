126a69dedaa6179daf031d0798f051219c32bb96
Leah <95726193+1eahjk@users.noreply.github.com>
2022-10-14 13:12:43 +0100

    If/Else

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/GuessMe.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/GuessMe.java
@@ -4,16 +4,21 @@ package com.mycompany.mavenproject1;
  *
  * @author leahjoneskirwin
  */
+
+import java.util.Scanner;
+       
 public class GuessMe {
    public static void main(String[] args) {
        
         int my_number;
-        my_number = 11;
+        my_number = 11; //This is the number to guess//
         
-        int your_number;
-        your_number = 10;
-  
+        int your_number; //This is user input guess//
+       
+    Scanner s = new Scanner(System.in);
+    
         System.out.println("I've chosen a number. Betcha can't guess it!!");
+        your_number = s.nextInt();
         System.out.println("Your number is: " + your_number);
         
 
